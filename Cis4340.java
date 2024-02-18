9be0881d5823dd24daa78eb8ce5a6ea8d5b05feb
sidneychristensen <160240476+sidneychristensen@users.noreply.github.com>
2024-02-18 16:24:24 -0500

    Update Cis4340.java

--- Cis4340.java
+++ Cis4340.java
@@ -17,7 +17,7 @@ public class Cis4340 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)");
+        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF (Sidney Christensen)");
     }
 
 }
