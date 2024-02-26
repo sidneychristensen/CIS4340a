0ce959c6029a90cb5f385eb28cb5aacade25681a
sidneychristensen <160240476+sidneychristensen@users.noreply.github.com>
2024-02-26 18:35:50 -0500

    Update R03_NUM09_J.java

--- R03_NUM09_J.java
+++ R03_NUM09_J.java
@@ -1,7 +1,8 @@
 //Rule 03: User example
 // NUM09-J
 
-for (float x = 0.1f; x <= 1.0f; x += 0.1f) {
+for (int count = 1; count <= 10; count += 1) {
+	float x = count/10.0f;
 	System.out.println(x);
 }
 
