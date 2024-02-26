0c2c7830ec6eaa0508b3b77ba49aaa918c515144
sidneychristensen <160240476+sidneychristensen@users.noreply.github.com>
2024-02-26 15:36:42 -0500

    Update and rename si234623-java4.java to R04_STR03_J.java

---
+++ R04_STR03_J.java
@@ -0,0 +1,7 @@
+//Rule 04: Characters and Strings (STR)
+//STR03-J
+BigInteger x = new BigInteger("530500452766");
+String s = x.toString(); //Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray);
+x = new BigInteger(ns);
