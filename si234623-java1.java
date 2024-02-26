f809756ce6998f3f89816ae1783f6388cb74a6ca
sidneychristensen <160240476+sidneychristensen@users.noreply.github.com>
2024-02-26 15:00:47 -0500

    Update si234623-java1.java

--- si234623-java1.java
+++ si234623-java1.java
@@ -1,7 +1,11 @@
 if (loginSuccessful) {
-	logger.severe("User login succeeded for: " + username);
+	logger.severe("User login succeeded for: " + sanitizeUser(username));
 }else{
-	logger.severe("Usr login failed for: " + username);
+	logger.severe("User login failed for: " + sanitizeUser(username));
+}
+public String sanitizeUser(String username) {
+	return Pattern.matches("[A-Za-z0-9_]+", username))
+	? username : "unauthorized user";
 }
 
  
